15568745be91ff6374cc92043906e5b028126c2d
SarahNaif <sarah.althowebi@gmail.com>
2022-12-21 15:42:04 +0300

    build

---
+++ udagram-api/www/controllers/v0/users/routes/auth.router.js
@@ -0,0 +1,98 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { Router } from 'express';
+import { User } from '../models/User.js';
+import * as c from '../../../../config/config.js';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import * as EmailValidator from 'email-validator';
+const router = Router();
+function generatePassword(plainTextPassword) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const saltRounds = 10;
+        const salt = yield bcrypt.genSalt(saltRounds);
+        return yield bcrypt.hash(plainTextPassword, salt);
+    });
+}
+function comparePasswords(plainTextPassword, hash) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return yield bcrypt.compare(plainTextPassword, hash);
+    });
+}
+function generateJWT(user) {
+    return jwt.sign(user.short(), c.config.jwt.secret);
+}
+export function requireAuth(req, res, next) {
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
+    const tokenBearer = req.headers.authorization.split(' ');
+    if (tokenBearer.length != 2) {
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+    const token = tokenBearer[1];
+    return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
+        if (err) {
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+        }
+        return next();
+    });
+}
+router.get('/verification', requireAuth, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.status(200).send({ auth: true, message: 'Authenticated.' });
+}));
+router.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (!email || !EmailValidator.validate(email)) {
+        return res.status(400).send({ auth: false, message: 'Email is required or malformed.' });
+    }
+    if (!password) {
+        return res.status(400).send({ auth: false, message: 'Password is required.' });
+    }
+    const user = yield User.findByPk(email);
+    if (!user) {
+        return res.status(401).send({ auth: false, message: 'User was not found..' });
+    }
+    const authValid = yield comparePasswords(password, user.passwordHash);
+    if (!authValid) {
+        return res.status(401).send({ auth: false, message: 'Password was invalid.' });
+    }
+    const jwt = generateJWT(user);
+    res.status(200).send({ auth: true, token: jwt, user: user.short() });
+}));
+router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const email = req.body.email;
+    const plainTextPassword = req.body.password;
+    if (!email || !EmailValidator.validate(email)) {
+        return res.status(400).send({ auth: false, message: 'Email is missing or malformed.' });
+    }
+    if (!plainTextPassword) {
+        return res.status(400).send({ auth: false, message: 'Password is required.' });
+    }
+    const user = yield User.findByPk(email);
+    if (user) {
+        return res.status(422).send({ auth: false, message: 'User already exists.' });
+    }
+    const generatedHash = yield generatePassword(plainTextPassword);
+    //@ts-ignore
+    const newUser = yield new User({
+        email: email,
+        passwordHash: generatedHash,
+    });
+    const savedUser = yield newUser.save();
+    const jwt = generateJWT(savedUser);
+    res.status(201).send({ token: jwt, user: savedUser.short() });
+}));
+router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send('auth');
+}));
+export const AuthRouter = router;
+//# sourceMappingURL=auth.router.js.map
---
+++ udagram-api/www/server.js
@@ -0,0 +1,49 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import express from "express";
+import { sequelize } from "./sequelize.js";
+import { IndexRouter } from "./controllers/v0/index.router.js";
+import bodyParser from "body-parser";
+import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index.js";
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    dotenv.config();
+    try {
+        yield sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    }
+    catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+    yield sequelize.addModels(V0_FEED_MODELS);
+    yield sequelize.addModels(V0_USER_MODELS);
+    yield sequelize.sync();
+    console.log("Database Connected");
+    const app = express();
+    const port = 8080;
+    app.use(bodyParser.json());
+    // app.use(cors());
+    // We set the CORS origin to * so that we don't need to
+    // worry about the complexities of CORS. 
+    app.use(cors());
+    app.use("/api/v0/", IndexRouter);
+    // Root URI call
+    app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        res.send("/api/v0/");
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`Backend server is listening on port ${port}....`);
+        console.log(`Frontent server running ${process.env.URL}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
